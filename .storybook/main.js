7b6a96c4de016408d2590b427ddb665a83ebacb9
Aziz Khilawala <aziz.khilawala@cohesity.com>
2020-04-20 11:25:13 -0700

    add files

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/app/components/**/*.stories.ts'],
+  addons: ['@storybook/addon-actions', 
+  '@storybook/addon-links', 
+  '@storybook/addon-notes',
+  '@storybook/addon-knobs',
+    '@storybook/addon-docs',
+  '@storybook/addon-designs']
+};
